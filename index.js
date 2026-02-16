e4120a7b8f2ea464ae890e90ded03ada5a4bfa09
M-Paez <157328953+M-Paez@users.noreply.github.com>
2026-02-15 19:47:09 -0600

    OnlyHopefully

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function rollDice() {
+    for(let i = 1; i<=5; i++) {
+        const roll = Math.floor(Math.random() * 6) + 1;
 
+        document.getElementById("die" + i).value = roll;
+        
+        document.getElementById("img" + i).src = "DiceImages/" + roll + ".png";
+    }
+}
